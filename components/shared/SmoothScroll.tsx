64cce61dcc040d043250c9db14792b4188573b5d
Md Anamul Hasan <anamul15-3625@diu.edu.bd>
2025-03-04 15:19:34 +0600

    Added logo

--- components/shared/SmoothScroll.tsx
+++ components/shared/SmoothScroll.tsx
@@ -5,33 +5,32 @@ import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 if (typeof window !== 'undefined') {
-    gsap.registerPlugin(ScrollTrigger)
+  gsap.registerPlugin(ScrollTrigger)
 }
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
-    useEffect(() => {
-        const lenis = new Lenis({
-            duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-            orientation: 'vertical',
-            lerp: 0.1,
-            smoothWheel: false,
-            touchMultiplier: 2,
-        })
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      touchMultiplier: 2,
+    })
 
-        lenis.on('scroll', ScrollTrigger.update)
+    // Integrate GSAP ScrollTrigger with Lenis
+    lenis.on('scroll', ScrollTrigger.update)
 
-        gsap.ticker.add((time) => {
-            lenis.raf(time * 1000)
-        })
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000)
+    })
 
-        gsap.ticker.lagSmoothing(0)
+    gsap.ticker.lagSmoothing(0)
 
-        return () => {
-            lenis.destroy()
-            gsap.ticker.remove(lenis.raf)
-        }
-    }, [])
+    return () => {
+      lenis.destroy()
+      gsap.ticker.remove(lenis.raf)
+    }
+  }, [])
 
-    return <>{children}</>
+  return <>{children}</>
 }
